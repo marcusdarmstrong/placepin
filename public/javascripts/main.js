2ed3818c599dfcbe38b3b0467ba2166710da787f
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-06-30 21:18:14 -0400

    Google map

---
+++ public/javascripts/main.js
@@ -0,0 +1,25 @@
+(function(init) {
+    init(window.jQuery, window._, window, document);
+}(function($, _, window, document) {
+    var doEventBinding = function(map) {
+    };
+    
+    var defaultOptions = function() {
+        return {
+            //zoom: 3,
+            //center: new google.maps.LatLng(0, 0),
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI: true/*,
+            styles: styles*/
+        };
+    };
+    
+    var initializeMap = function(options) {
+        var map = new google.maps.Map($('#map')[0], options);
+        doEventBinding(map);
+    };
+    
+    $(function() {
+        initializeMap(defaultOptions());
+    });
+}));
