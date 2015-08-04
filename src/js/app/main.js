6e6f2c7a20b38ce9f9a43554b1d61a6dbb22f021
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-08-04 17:50:17 -0400

    Updating map styles

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -6,7 +6,23 @@
       zoom: 5,
       center: new window.google.maps.LatLng(39, -97),
       mapTypeId: window.google.maps.MapTypeId.ROADMAP,
-      /*styles: styles*/
+      styles: [
+        {
+          "stylers": [
+            { "saturation": -50 }
+          ]
+        },{
+          "featureType": "poi.park",
+          "stylers": [
+            { "saturation": 15 }
+          ]
+        },{
+          "featureType": "road",
+          "stylers": [
+            { "visibility": "simplified" }
+          ]
+        }
+      ],
       panControl: false,
       mapTypeControl: false,
       scaleControl: true,
